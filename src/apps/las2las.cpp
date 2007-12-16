fcb2b24ee78fa0c098a2473a6bd975cbe5ddc349
Howard Butler <hobu.inc@gmail.com>
2007-12-16 02:37:02 +0000

    propsets
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%4086

--- src/apps/las2las.cpp
+++ src/apps/las2las.cpp
@@ -13,9 +13,9 @@
 
 
 #include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 #include "lasreader.h"
 #include "laswriter.h"
