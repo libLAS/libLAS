f1c3b0050d9f18e1e6b5d06a424ed2bc0573ec09
Mateusz Loskot <mateusz@loskot.net>
2007-12-17 07:06:58 +0000

    include\laspoint.hpp: moved ClassificationType into Point class, moved definition of inline functions outside the class definition. Created src\laspoint.cpp file for big-ass function bodies.
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40117

--- include/laspoint.hpp
+++ include/laspoint.hpp
@@ -18,40 +18,71 @@
 
 #include <exception>
 #include <lastypes.hpp>
-namespace liblas {
 
-enum ClassificationType
+namespace liblas
 {
-    eCreated = 0,
-    eUnclassified,
-    eGround,
-    eLowVegetation,
-    eHighVegetation,
-    eBuilding,
-    eLowPoint,
-    eModelKeyPoint,
-    eWater,
-    eOverlapPoints = 12
-};
 
 class Point
 {
 public:
+
+    enum ClassificationType
+    {
+        eCreated = 0,
+        eUnclassified,
+        eGround,
+        eLowVegetation,
+        eHighVegetation,
+        eBuilding,
+        eLowPoint,
+        eModelKeyPoint,
+        eWater,
+        eOverlapPoints = 12
+    };
+
     Point(float nfX, float nfY, float nfZ, float nfIntensity, double ndTime);
-    float GetX() const { return nfX; }
-    float GetY() const { return nfY; }
-    float GetZ() const { return nfZ; }
-    float GetIntensity() const { return nfIntensity; }
-    double GetTime() const { return ndGPSTime; }
-    ClassificationType GetClassification() const { return nClassification; }
+
+    float GetX() const;
+    float GetY() const;
+    float GetZ() const;
+    float GetIntensity() const;
+    double GetTime() const;
+    ClassificationType GetClassification() const;
+
 private:
-    float nfX;
-    float nfY;
-    float nfZ;
-    float nfIntensity;
-    double ndGPSTime;
-    ClassificationType nClassification;
+    
+    float m_nfX;
+    float m_nfY;
+    float m_nfZ;
+    float m_nfIntensity;
+    double m_ndGPSTime;
+    ClassificationType m_nClassification;
 };
+
+inline float Point::GetX() const
+{
+    return m_nfX;
+}
+inline float Point::GetY() const
+{
+    return m_nfY;
+}
+inline float Point::GetZ() const
+{
+    return m_nfZ;
+}
+inline float Point::GetIntensity() const
+{
+    return m_nfIntensity;
+}
+inline double Point::GetTime() const
+{
+    return m_ndGPSTime;
+}
+inline Point::ClassificationType Point::GetClassification() const
+{
+    return m_nClassification;
+}
 
 }; /* end namespace liblas */
 
---
+++ src/laspoint.cpp
@@ -0,0 +1,33 @@
+/***************************************************************************
+ * $Id: laspoint.hpp 109 2007-12-17 06:53:23Z mloskot $
+ * $Date: 2007-12-17 07:53:23 +0100 (Mon, 17 Dec 2007) $
+ *
+ * Project: libLAS -- C/C++ read/write library for LAS LIDAR data
+ * Purpose: LAS Point class, and appropriate enumerations that go with. 
+ * Author:  Phil Vachon <philippe@cowpig.ca>
+ *
+ ***************************************************************************
+ * Copyright (c) 2007, Martin Isenburg <isenburg@cs.unc.edu>
+ * Copyright (c) 2007, Phil Vachon <philippe@cowpig.ca> 
+ *
+ * See LICENSE.txt in this source distribution for more information.
+ **************************************************************************/
+
+#ifndef LASPOINT_HPP_INCLUDED
+#define LASPOINT_HPP_INCLUDED
+
+#include <exception>
+#include <lastypes.hpp>
+
+namespace liblas
+{
+
+Point::Point(float nfX, float nfY, float nfZ, float nfIntensity, double ndTime)
+    : m_nfX(nfX), m_nfY(nfY), m_nfZ(nfZ), m_nfIntensity(nfIntensity), m_ndGPSTime(ndTime)
+{
+}
+
+
+}; /* end namespace liblas */
+
+#endif /* ndef LASPOINT_HPP_INCLUDED */
