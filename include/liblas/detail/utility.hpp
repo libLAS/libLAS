81bd1a870129b3e0bf4c8fe43df813e4a4ca9b3c
Mateusz Loskot <mateusz@loskot.net>
2008-01-16 22:10:04 +0000

    Cleaned mess after incorrectly resolved conflict. Removed <iostream> as not necessary because forward declarations from <iosfwd> are sufficient and faster to compile.
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40221

--- include/liblas/detail/utility.hpp
+++ include/liblas/detail/utility.hpp
@@ -2,12 +2,8 @@
 #define LIBLAS_DETAIL_UTILITY_HPP_INCLUDED
 
 #include <iosfwd>
-<<<<<<< .mine
 #include <sstream> // std::ostringstream
-=======
 #include <stdexcept>
-#include <iostream>
->>>>>>> .r219
 
 namespace liblas { namespace detail {
 
@@ -39,7 +35,6 @@ inline char* bytes_of(T* data)
     return static_cast<char*>(static_cast<void*>(data));
 }
 
-<<<<<<< .mine
 template <typename T>
 void read_n(T& dest, std::istream& input, std::streamsize const& num)
 {
@@ -67,33 +62,6 @@ void read_n(T& dest, std::istream& input, std::streamsize const& num)
         os << "read only " << rn << " bytes of " << num;
         throw std::runtime_error(os.str());
     }
-=======
-template <typename T>
-void read_n(T& dest, std::istream& input, std::streamsize const& num)
-{
-    // TODO: Review and redesign errors handling logic if necessary
-
-    if (!input)
-        throw std::runtime_error("input stream is not readable");
-
-    // Read bytes into buffer
-    input.read(detail::bytes_of(dest), num);
-    
-    // Test stream state bits
-    if (input.eof())
-        throw std::runtime_error("end of file encountered");
-    else if (input.fail())
-        throw std::runtime_error("non-fatal I/O error occured");
-    else if (input.bad())
-        throw std::runtime_error("fatal I/O error occured");
-
-    // Poor man test of data consistency
-    std::streamsize const rn = input.gcount();
-    if (num != rn)
-    {
-        throw std::runtime_error("read only " + rn + std::string("bytes of " + num));
-    }
->>>>>>> .r219
 }
 
 }} // namespace liblas::detail
