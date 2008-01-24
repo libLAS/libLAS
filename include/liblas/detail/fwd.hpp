ad9ac50de2fd38fe9d75e3d0ae6570f6962cf19a
Mateusz Loskot <mateusz@loskot.net>
2008-01-24 00:07:02 +0000

    Added detail/fwd.hpp header gathering common forward declarations used in the implementation. This is a private header and does not belong to public interface.
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40279

---
+++ include/liblas/detail/fwd.hpp
@@ -0,0 +1,18 @@
+#ifndef LIBLAS_DETAIL_FWD_HPP_INCLUDED
+#define LIBLAS_DETAIL_FWD_HPP_INCLUDED
+
+// Forward declarations
+namespace liblas {
+
+class LASHeader;
+class LASPoint;
+
+namespace detail {
+
+class Reader;
+class Writer;
+struct PointRecord;
+
+}} // namespace liblas::detail
+
+#endif // LIBLAS_DETAIL_FWD_HPP_INCLUDED
