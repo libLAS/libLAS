a56fe55411f32e7fe3f8e88ac27c43b15c84f439
Mateusz Loskot <mateusz@loskot.net>
2008-01-31 03:07:53 +0000

    Fixed stupid bugs with incorrectly calculated size of arrays in memset/memcpy calls in LASPoint ctors.
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40294

--- src/laspoint.cpp
+++ src/laspoint.cpp
@@ -10,17 +10,15 @@ namespace liblas {
 LASPoint::LASPoint() :
     m_intensity(0), m_flags(0), m_class(0), m_gpsTime(0)
 {
-    std::memset(m_coords, 0, coords_size);
+    std::memset(m_coords, 0, sizeof(m_coords));
 }
 
 LASPoint::LASPoint(LASPoint const& other) :
     m_intensity(other.m_intensity),
         m_flags(other.m_flags), m_class(other.m_class),
             m_gpsTime(other.m_gpsTime)
-{    
-    m_coords[0] = other.m_coords[0];
-    m_coords[1] = other.m_coords[1];
-    m_coords[2] = other.m_coords[2];
+{
+    std::memcpy(m_coords, other.m_coords, sizeof(m_coords));
 }
 
 LASPoint& LASPoint::operator=(LASPoint const& rhs)
