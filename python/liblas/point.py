3bb7956aaf8d078af5a09c21495dd83ce46e71a0
Howard Butler <hobu.inc@gmail.com>
2008-04-09 18:03:35 +0000

    Implement tests for LASPoint python wrapper
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40552

--- python/liblas/point.py
+++ python/liblas/point.py
@@ -11,7 +11,8 @@ class Point(object):
         self.owned = owned
     def __del__(self):
         if self.owned:
-            core.las.LASPoint_Destroy(self.handle)
+            if self.handle and core:
+                core.las.LASPoint_Destroy(self.handle)
     
     def get_x(self):
         """Returns the X coordinate of the LAS point. 
@@ -80,15 +81,23 @@ class Point(object):
     scan_direction = property(get_scan_direction, set_scan_direction)
     
     def get_flightline_edge(self):
+        """Denotes whether the point is a flight line edge"""
         return core.las.LASPoint_GetFlightLineEdge(self.handle)
     def set_flightline_edge(self, value):
-        """Sets the flightline edge as an integer for the point"""
+        """Sets the flightline edge as an integer for the point.  Must be 0 (not an edge) or 1 (an edge)"""
         core.las.LASPoint_SetFlightLineEdge(self.handle, value)
     flightline_edge = property(get_flightline_edge, set_flightline_edge)
     
     def get_scan_flags(self):
+        """Returns the raw scan flags for the point.  
+        See the LAS 1.0 or 1.1 specification for information how to interpret
+        """
         return core.las.LASPoint_GetScanFlags(self.handle)
     def set_scan_flags(self, value):
+        """Sets the raw scan flags for the point.
+        See the LAS 1.0 or 1.1 specification for information how to interpret or 
+        use the convenience functions like flightline_edge, scan_direction, etc.
+        """
         core.las.LASPoint_SetScanFlags(self.handle, value)
     scan_flags = property(get_scan_flags, set_scan_flags)
     
@@ -99,14 +108,16 @@ class Point(object):
     classification = property(get_classification, set_classification)
     
     def get_scan_angle_rank(self):
+        """Returns the scan angle rank of the point.  It will be between -90 and 90"""
         return core.las.LASPoint_GetScanAngleRank(self.handle)
     def set_scan_angle_rank(self, value):
+        """Sets the scan angle rank of the point.  It must be between -90 and 90"""
         core.las.LASPoint_SetScanAngleRank(self.handle, value)
     scan_angle = property(get_scan_angle_rank, set_scan_angle_rank)
 
     def get_user_data(self):
         return core.las.LASPoint_GetUserData(self.handle)
-    def set_user_data(self):
+    def set_user_data(self, value):
         core.las.LASPoint_SetUserData(self.handle, value)
     user_data = property(get_user_data, set_user_data)
 
@@ -114,6 +125,20 @@ class Point(object):
         t = core.las.LASPoint_GetTime(self.handle)
         return datetime.datetime(*time.localtime(t)[0:7])
     def set_time(self, value):
+        """
+        Sets the time of the point from a datetime value
+
+        Parameters
+        ----------
+        value : datetime.datetime instance or none to use the current time
+
+        Example
+        -------
+        >>> t = datetime.datetime(2008,3,19)
+        >>> p.time = t
+        >>> p.time
+        datetime.datetime(2008, 3, 19, 0, 0)
+        """
         t = time.mktime(value.timetuple())
         core.las.LASPoint_SetTime(self.handle,t)
     time = property(get_time, set_time)
