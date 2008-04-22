475dbdd18c5e8d60bd3a458b64762cbe346f7130
Mateusz Loskot <mateusz@loskot.net>
2008-04-22 22:31:53 +0000

    Added new cases to LASReader test.
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40582

--- test/unit/lasreader_test.cpp
+++ test/unit/lasreader_test.cpp
@@ -7,6 +7,7 @@
 //
 #include <liblas/lasreader.hpp>
 #include <liblas/lasheader.hpp>
+#include <liblas/laspoint.hpp>
 #include <liblas/cstdint.hpp>
 #include <liblas/liblas.hpp>
 #include <tut/tut.hpp>
@@ -85,5 +86,78 @@ namespace tut
         ensure_equals(hdr.GetMaxZ(), double(55.26));
     }
 
+    // Test GetPoint method
+    template<>
+    template<>
+    void to::test<3>()
+    {
+        std::ifstream ifs;
+        ifs.open(file10_.c_str(), std::ios::in | std::ios::binary);
+        liblas::LASReader reader(ifs);
+
+        // uninitialized point object, a null-point
+        liblas::LASPoint const& p = reader.GetPoint();
+        ensure(p == liblas::LASPoint());
+    }
+
+    // Test ReadPoint and GetPoint pair
+    template<>
+    template<>
+    void to::test<4>()
+    {
+        std::ifstream ifs;
+        ifs.open(file10_.c_str(), std::ios::in | std::ios::binary);
+        liblas::LASReader reader(ifs);
+
+        // read 1st point
+        reader.ReadNextPoint();
+        liblas::LASPoint const& p1 = reader.GetPoint();
+        ensure_distance(p1.GetX(), double(630262.30), 0.0001);
+        ensure_distance(p1.GetY(), double(4834500), 0.0001);
+        ensure_distance(p1.GetZ(), double(51.53), 0.0001);
+        ensure_equals(p1.GetIntensity(), 670);
+        ensure_equals(p1.GetClassification(), 1);
+		ensure_equals(p1.GetScanAngleRank(), 0);
+		ensure_equals(p1.GetUserData(), 0);
+        ensure_equals(p1.GetScanFlags(), 9);
+		ensure_distance(p1.GetTime(), double(413665.23360000004), 0.0001);
+
+        // read 2nd point
+        reader.ReadNextPoint();
+        liblas::LASPoint const& p2 = reader.GetPoint();
+        ensure_distance(p2.GetX(), double(630282.45), 0.0001);
+        ensure_distance(p2.GetY(), double(4834500), 0.0001);
+        ensure_distance(p2.GetZ(), double(51.63), 0.0001);
+        ensure_equals(p2.GetIntensity(), 350);
+        ensure_equals(p2.GetClassification(), 1);
+		ensure_equals(p2.GetScanAngleRank(), 0);
+		ensure_equals(p2.GetUserData(), 0);
+        ensure_equals(static_cast<unsigned int>(p2.GetScanFlags()), 9);
+		ensure_distance(p2.GetTime(), double(413665.52880000003), 0.0001);
+
+        // read and skip 3rd point
+        reader.ReadNextPoint();
+
+        // read 4th point
+        reader.ReadNextPoint();
+        liblas::LASPoint const& p3 = reader.GetPoint();
+        ensure_distance(p3.GetX(), double(630346.83), 0.0001);
+        ensure_distance(p3.GetY(), double(4834500), 0.0001);
+        ensure_distance(p3.GetZ(), double(50.90), 0.0001);
+        ensure_equals(p3.GetIntensity(), 150);
+        ensure_equals(p3.GetClassification(), 1);
+		ensure_equals(p3.GetScanAngleRank(), 0);
+		ensure_equals(p3.GetUserData(), 0);
+        ensure_equals(static_cast<unsigned int>(p3.GetScanFlags()), 18);
+		ensure_distance(p3.GetTime(), double(414093.84360000002), 0.0001);
+
+        // read and count remaining points from 5 to 8
+        unsigned short c = 0;
+        while (reader.ReadNextPoint())
+        {
+            ++c;
+        }
+        ensure_equals(c, 4);
+    }
 }
 
