f426d15b3e6a60dcb54c9c0f723123fb4fb18d16
Mateusz Loskot <mateusz@loskot.net>
2008-04-22 21:53:15 +0000

    Added test data path as command line argument of liblas_test program. Added liblas:LASReader test.
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40580

--- test/unit/liblas_test_suite.cpp
+++ test/unit/liblas_test_suite.cpp
@@ -8,14 +8,26 @@
 #include <tut/tut.hpp>
 #include <tut/tut_reporter.hpp>
 #include <iostream>
+#include <string>
 
 namespace tut
 {
     test_runner_singleton runner;
+
+    // full path to trunk/test/data
+    std::string g_test_data_path;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (2 != argc)
+    {
+        std::cout << "Usage: liblas_test <test data path>\n";
+        return 1;
+    }
+
+    tut::g_test_data_path = argv[1];
+
     tut::reporter reporter;
     tut::runner.get().set_callback(&reporter);
 
