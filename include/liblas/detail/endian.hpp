8f51af19a443c2ec8e416822df1dc44061cf3a37
Howard Butler <hobu.inc@gmail.com>
2008-05-02 21:26:15 +0000

    license additions
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40652

--- include/liblas/detail/endian.hpp
+++ include/liblas/detail/endian.hpp
@@ -1,16 +1,30 @@
-// $Id$
-//
-// This file has been stolen from <boost/detail/endian.hpp> and
-// modified for libLAS purposes.
-//
-// (C) Copyright Mateusz Loskot 2007, mateusz@loskot.net
-// (C) Copyright Caleb Epstein 2005
-// (C) Copyright John Maddock 2006
-// Distributed under the Boost  Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-
-/*
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  libLAS - http://liblas.org - A BSD library for LAS format data.
+ * Purpose:  Endian macros
+ * Author:   Mateusz Loskot, mateusz@loskot.net
+ *
+ * This file has been stolen from <boost/cstdint.hpp> and
+ * modified for libLAS purposes.
+ * 
+ * (C) Copyright Mateusz Loskot 2007, mateusz@loskot.net
+ * (C) Copyright Caleb Epstein 2005
+ * (C) Copyright John Maddock 2006
+ * Distributed under the Boost  Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ * 
+ * Revision History
+ * 06 Feb 2006 - Initial Revision
+ * 09 Nov 2006 - fixed variant and version bits for v4 guids
+ * 13 Nov 2006 - added serialization
+ * 17 Nov 2006 - added name-based guid creation
+ * 20 Nov 2006 - add fixes for gcc (from Tim Blechmann)
+ * 07 Mar 2007 - converted to header only
+ * 20 Jan 2008 - removed dependency of Boost and modified for libLAS (by Mateusz Loskot)
+ ******************************************************************************
+ *
  * Copyright (c) 1997
  * Silicon Graphics Computer Systems, Inc.
  *
@@ -21,15 +35,18 @@
  * in supporting documentation.  Silicon Graphics makes no
  * representations about the suitability of this software for any
  * purpose.  It is provided "as is" without express or implied warranty.
- */
-
-/*
+ *
+ * 
+ *
  * Copyright notice reproduced from <boost/detail/limits.hpp>, from
  * which this code was originally taken.
  *
  * Modified by Caleb Epstein to use <endian.h> with GNU libc and to
  * defined the BOOST_ENDIAN macro.
- */
+ ****************************************************************************/
+
+
+
 #ifndef LIBLAS_DETAIL_ENDIAN_HPP_INCLUDED
 #define LIBLAS_DETAIL_ENDIAN_HPP_INCLUDED
 
