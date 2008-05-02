826ad597ef9c204eb4d56305a24e50de801f90a8
Howard Butler <hobu.inc@gmail.com>
2008-05-02 21:14:18 +0000

    license additions and propsets
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40651

--- include/liblas/guid.hpp
+++ include/liblas/guid.hpp
@@ -1,24 +1,60 @@
-// $Id$
-//
-// This file has been stolen from <boost/guid.hpp> file
-// available at http://www.boost-consulting.com/vault/
-// and modified for libLAS purposes.
-//
-// (C) Copyright 2006 Andy Tompkins.
-// (C) Copyright 2008 Mateusz Loskot, mateusz@loskot.net.
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-//
-// Revision History
-//  06 Feb 2006 - Initial Revision
-//  09 Nov 2006 - fixed variant and version bits for v4 guids
-//  13 Nov 2006 - added serialization
-//  17 Nov 2006 - added name-based guid creation
-//  20 Nov 2006 - add fixes for gcc (from Tim Blechmann)
-//  07 Mar 2007 - converted to header only
-//  20 Jan 2008 - removed dependency of Boost and modified for libLAS (by Mateusz Loskot)
-//
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  libLAS - http://liblas.org - A BSD library for LAS format data.
+ * Purpose:  GUID implementation
+ * Author:   Mateusz Loskot, mateusz@loskot.net
+ *
+ * This file has been stolen from <boost/cstdint.hpp> and
+ * modified for libLAS purposes.
+ * 
+ * (C) Copyright 2006 Andy Tompkins.
+ * (C) Copyright 2008 Mateusz Loskot, mateusz@loskot.net.
+ * Distributed under the Boost  Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ * 
+ * Revision History
+ * 06 Feb 2006 - Initial Revision
+ * 09 Nov 2006 - fixed variant and version bits for v4 guids
+ * 13 Nov 2006 - added serialization
+ * 17 Nov 2006 - added name-based guid creation
+ * 20 Nov 2006 - add fixes for gcc (from Tim Blechmann)
+ * 07 Mar 2007 - converted to header only
+ * 20 Jan 2008 - removed dependency of Boost and modified for libLAS (by Mateusz Loskot)
+ ******************************************************************************
+ *
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following 
+ * conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright 
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright 
+ *       notice, this list of conditions and the following disclaimer in 
+ *       the documentation and/or other materials provided 
+ *       with the distribution.
+ *     * Neither the name of the Martin Isenburg or Iowa Department 
+ *       of Natural Resources nor the names of its contributors may be 
+ *       used to endorse or promote products derived from this software 
+ *       without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
+ ****************************************************************************/
+
 #ifndef LIBLAS_GUID_HPP_INCLUDED
 #define LIBLAS_GUID_HPP_INCLUDED
 
