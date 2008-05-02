27295694911a117acff1dc903f0038932ecee49a
Mateusz Loskot <mateusz@loskot.net>
2008-05-02 05:03:33 +0000

    Added test for liblas::LASError class (Ticket #30).
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40643

--- include/liblas/laserror.hpp
+++ include/liblas/laserror.hpp
@@ -16,23 +16,35 @@
 
 namespace liblas {
 
-/// \todo To be documented.
+/// Definition of error notification used on the level of C API.
+/// This class describes details of error condition occured in
+/// libLAS core. All errors are stacked by C API layer, so it's
+/// possible to track problem down to its source.
 class LASError
 {
 public:
 
+    /// Custom constructor.
+    /// This is the main and the only tool to initialize error instance.
     LASError(int code, std::string const& message, std::string const& method);
+
+    /// Copy constructor.
+    /// Error objects are copyable.
     LASError(LASError const& other);
+
+    /// Assignment operator.
+    /// Error objects are assignable.
     LASError& operator=(LASError const& rhs);
 
     // TODO - mloskot: What about replacing string return by copy with const char* ?
     //        char const* GetMethod() const { return m_method.c_str(); }, etc.
 
-    std::string GetMethod() const { return m_method; };
+    int GetCode() const { return m_code; };
     std::string GetMessage() const { return m_message; };
-    int GetCode() const {return m_code; };
+    std::string GetMethod() const { return m_method; };    
 
 private:
+
     int m_code;
     std::string m_message;
     std::string m_method;
