0835ef59bd0833cfa16a6d41407bd305f005186e
Howard Butler <hobu.inc@gmail.com>
2008-05-03 06:06:21 +0000

    doc updates
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40657

--- include/liblas/capi/liblas.h
+++ include/liblas/capi/liblas.h
@@ -115,14 +115,14 @@ LAS_DLL LASError LASError_GetLastErrorNum(void);
 /// Returns the error message of the last error on the error stack.
 LAS_DLL char * LASError_GetLastErrorMsg(void);
 
-/// Returns the name of the method the last error message happened in/
+/// Returns the name of the method the last error message happened in
 LAS_DLL char * LASError_GetLastErrorMethod(void);
 
 /// Returns the number of error messages on the error stack.
 LAS_DLL int LASError_GetErrorCount(void);
 
 /// Prints the last error message in the error stack to stderr.  If 
-/// there is no error on the error stack, only the @param message is printed.
+/// there is no error on the error stack, only the message is printed.
 /// The function does not alter the error stack in any way.
 /// @param message Message to include in the stderr output
 LAS_DLL void LASError_Print(const char* message);
@@ -130,6 +130,7 @@ LAS_DLL void LASError_Print(const char* message);
 /// Creates a LASReaderH object that can be used to read LASHeaderH and 
 /// LASPointH objects with.  The LASReaderH must not be created with a 
 /// filename that is opened for read or write by any other API functions.
+/// @return LASReaderH opaque pointer to a LASReader instance.
 /// @param filename Filename to open for read 
 LAS_DLL LASReaderH LASReader_Create(const char * filename);
 
