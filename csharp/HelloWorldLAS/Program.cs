5f50dca0f59f66ffddf94e27aadf7dee8410f0fe
Martin Vales <mrodriguez@stereocarto.com>
2008-09-15 06:55:42 +0000

    --HG-- extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40870

---
+++ csharp/HelloWorldLAS/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using System.Text;
+
+using LibLAS;
+
+
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+
+        try
+        {
+        LASReader lasreader = new LASReader(@"c:\las\sample_our2.las");
+
+        LASPoint laspoint;
+
+        LASHeader lasheader = lasreader.GetHeader();
+        Console.WriteLine(lasheader.SoftwareId);// 
+        lasheader.VersionMinor = 0; 
+        LASWriter laswriter = new LASWriter(@"c:\las\sample_our.las", lasheader, LASReadWriteMode.LASModeWrite);
+
+        Console.WriteLine("Number of points in file= {0}", lasheader.PointRecordsCount);
+
+        while (lasreader.GetNextPoint())
+        {
+
+            laspoint = lasreader.GetPoint();
+            laspoint.X = laspoint.X + 3;
+
+            //Console.WriteLine(laspoint.X + "," + laspoint.Y + "," + laspoint.Z);
+
+            laswriter.WritePoint(laspoint);
+
+        }
+
+        }
+        catch (LASException e)
+        {
+            Console.WriteLine("\nLASException! Msg: {0}", e.Message);
+        }
+        catch
+        {
+            Console.WriteLine("Unknown exception caught");
+        }
+        finally
+        {
+            Console.WriteLine("Do i need something to do?");
+        }
+        Console.WriteLine("End of file");
+        Console.Read();
+    }
+}
+
---
+++ csharp/NUnitTest/LASFilesClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTest
+{
+     class LASFilesClass
+    {
+        static public string folder = @"F:\test\data\";
+        static public string TO_core_last_zoom = "TO_core_last_zoom";
+    }
+}
