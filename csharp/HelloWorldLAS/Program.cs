0086ce4c7d513d620b2e0967281ff3ef5f037221
Mateusz Loskot <mateusz@loskot.net>
2008-10-09 12:30:23 +0000

    trunk/csharp: Catch SystemException in HelloWorldLAS to diagnose DLL Not Found problems.
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40905

--- csharp/HelloWorldLAS/Program.cs
+++ csharp/HelloWorldLAS/Program.cs
@@ -1,48 +1,41 @@
 ﻿using System;
-
 using System.Text;
-
 using LibLAS;
-
-
 
 class Program
 {
-
-
     static void Main(string[] args)
     {
-
-
         try
         {
-        LASReader lasreader = new LASReader(@"c:\las\sample_our2.las");
+            LASReader lasreader = new LASReader(@"c:\las\sample_our2.las");
 
-        LASPoint laspoint;
+            LASPoint laspoint;
 
-        LASHeader lasheader = lasreader.GetHeader();
-        Console.WriteLine(lasheader.SoftwareId);// 
-        lasheader.VersionMinor = 0; 
-        LASWriter laswriter = new LASWriter(@"c:\las\sample_our.las", lasheader, LASReadWriteMode.LASModeWrite);
+            LASHeader lasheader = lasreader.GetHeader();
+            Console.WriteLine(lasheader.SoftwareId);// 
+            lasheader.VersionMinor = 0;
+            LASWriter laswriter = new LASWriter(@"c:\las\sample_our.las", lasheader, LASReadWriteMode.LASModeWrite);
 
-        Console.WriteLine("Number of points in file= {0}", lasheader.PointRecordsCount);
+            Console.WriteLine("Number of points in file= {0}", lasheader.PointRecordsCount);
 
-        while (lasreader.GetNextPoint())
-        {
+            while (lasreader.GetNextPoint())
+            {
+                laspoint = lasreader.GetPoint();
+                laspoint.X = laspoint.X + 3;
 
-            laspoint = lasreader.GetPoint();
-            laspoint.X = laspoint.X + 3;
+                //Console.WriteLine(laspoint.X + "," + laspoint.Y + "," + laspoint.Z);
 
-            //Console.WriteLine(laspoint.X + "," + laspoint.Y + "," + laspoint.Z);
-
-            laswriter.WritePoint(laspoint);
-
-        }
-
+                laswriter.WritePoint(laspoint);
+            }
         }
         catch (LASException e)
         {
             Console.WriteLine("\nLASException! Msg: {0}", e.Message);
+        }
+        catch (SystemException e)
+        {
+            Console.WriteLine("\nException! Msg: {0}", e.Message);
         }
         catch
         {
@@ -56,4 +49,3 @@ class Program
         Console.Read();
     }
 }
-
