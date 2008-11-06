6c4209c4ea8cac3036ff59216c4776e07e9565a4
Mateusz Loskot <mateusz@loskot.net>
2008-11-06 15:51:07 +0000

    test/unit/common.hpp: Added is_xy predicated and bbox_calculator functor, both used in unit tests.
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40960

--- test/unit/common.hpp
+++ test/unit/common.hpp
@@ -13,6 +13,58 @@
 namespace tut
 {
 
+// Predicate testing LASPoint against given XY coordinates
+// and tolerance.
+struct is_xy
+{
+    is_xy(double x, double y, double tolerance)
+        : x(x), y(y), t(tolerance)
+    {}
+
+    bool operator()(liblas::LASPoint const& p)
+    {
+        double const dx = x - p.GetX();
+        double const dy = y - p.GetY();
+
+        return ((dx <= t && dx >= -t) && (dy <= t && dy >= -t));
+    }
+
+    double x;
+    double y;
+    double t;
+};
+
+// Functor to calculate bounding box of a set of points
+struct bbox_calculator
+{
+    bbox_calculator(liblas::detail::Extents<double>& bbox)
+        : empty(true), bbox(bbox)
+    {}
+
+    void operator()(liblas::LASPoint const& p)
+    {
+        // Box initialization during first iteration only
+        if (empty)
+        {
+            bbox.min.x = bbox.max.x = p.GetX();
+            bbox.min.y = bbox.max.y = p.GetY();
+            bbox.min.z = bbox.max.z = p.GetZ();
+            empty = false;
+        }
+
+        // Expand bounding box to include given point
+        bbox.min.x = std::min(bbox.min.x, p.GetX());
+        bbox.min.y = std::min(bbox.min.y, p.GetY());
+        bbox.min.z = std::min(bbox.min.z, p.GetZ());
+        bbox.max.x = std::max(bbox.max.x, p.GetX());
+        bbox.max.y = std::max(bbox.max.y, p.GetY());
+        bbox.max.z = std::max(bbox.max.z, p.GetZ());
+    }
+
+    bool empty;
+    liblas::detail::Extents<double>& bbox;
+};
+
 // Common test procedure for default constructed point data.
 void test_default_point(liblas::LASPoint const& p);
 
