5746d38bc6b208d7858052a3e5037b41ae630ce6
Mateusz Loskot <mateusz@loskot.net>
2009-02-03 14:50:12 +0000

    Added print_point() function to sample/utility.hpp header.
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%40986

--- test/sample/utility.hpp
+++ test/sample/utility.hpp
@@ -33,9 +33,59 @@ inline std::ostream& operator<<(std::ostream& os, liblas::LASPoint const& p)
        << "\nnor: " << p.GetNumberOfReturns()
        << "\nsd: " << p.GetScanDirection()
        << "\neofl: " << p.GetFlightLineEdge()
-       << "\ncls: " << p.GetClassification()
-       << "\n";
+       << "\ntime: " << p.GetTime()
+       << std::endl;
+
     return os;
+}
+
+inline void print_point(std::ostream& os, liblas::LASPoint const& p, liblas::uint8_t const& minor)
+{
+    os << std::fixed << std::setprecision(6)
+       << "\nx: " << p.GetX()
+       << "\ny: " << p.GetY()
+       << "\nz: " << p.GetZ()
+       << "\nint: " << p.GetIntensity()
+       << "\nrn: " << p.GetReturnNumber()
+       << "\nnor: " << p.GetNumberOfReturns()
+       << "\nsd: " << p.GetScanDirection()
+       << "\neofl: " << p.GetFlightLineEdge()
+       << "\ntime: " << p.GetTime();
+
+    if (1 == minor)
+    {
+        const int len = 13;
+        static std::string meaning[len] =
+        {
+            "Created, never classified",
+            "Unclassified",
+            "Ground",
+            "Low Vegetation",
+            "Medium Vegetation",
+            "High Vegetation",
+            "Building",
+            "Low Point (noise)",
+            "Model Key-point (mass point)",
+            "Water",
+            "Reserved for ASPRS Definition",
+            "Reserved for ASPRS Definition",
+            "Overlap Points",
+        };
+
+        // bit 0:4
+        liblas::uint8_t chClass = p.GetClassification();
+        unsigned int idx = (unsigned int)((chClass >> 3) & 0x1F); 
+        std::cout << "\nClassification: "
+            << ((idx >= 0 && idx < len) ? meaning[idx] : "Reserved for ASPRS Definition");
+        std::cout << "\n- Synthetic: " << ((1 == (short)((chClass >> 2) & 0x1)) ? -1 : 0);
+        std::cout << "\n- Model_Key_Point: " << ((1 == (short)((chClass >> 1) & 0x1)) ? -1 : 0);
+        std::cout << "\n- Withheld: " << ((1 == (short)(chClass & 0x1)) ? -1 : 0);
+    }
+    else
+    {
+       std::cout << "\ncls: " << (int)p.GetClassification();
+    }
+    std::cout << std::endl;
 }
 
 #endif // LIBLAS_SAMPLE_UTILITY_HPP_INCLUDED
