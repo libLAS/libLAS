e2ee32c6754d9bf9245067f5fe0ae46c7bd4199e
Howard Butler <hobu.inc@gmail.com>
2009-03-04 19:36:59 +0000

    add number_of_points parameter
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%401081

--- test/data/make_data.py
+++ test/data/make_data.py
@@ -31,6 +31,7 @@ s = srs.SRS()
 s.proj4 = '+proj=utm +zone=15 +ellps=NAD83 +datum=NAD83 +units=m +no_defs '
 
 g = guid.GUID(key='8388f1b8-aa1b-4108-bca3-6bc68e7b062e')
+number_of_points = 1
 
 def write_file(version, format):
     h = header.Header()
@@ -41,12 +42,13 @@ def write_file(version, format):
     h.minor_version = version
     h.min = [p.x, p.y, p.z]
     h.max = [p.x, p.y, p.z]
-    h.point_return_count = [0L, 1L, 0L, 0L, 0L, 0L, 0L, 0L]
+    h.point_return_count = [0L, number_of_points, 0L, 0L, 0L, 0L, 0L, 0L]
     h.srs = s
     h.date = p.time
     
     f = file.File('1.%d_%d.las'%(version,format), mode='w', header=h)
-    f.write(p)
+    for i in xrange(number_of_points):
+        f.write(p)
     f.close()
 
 write_file(0,0)
