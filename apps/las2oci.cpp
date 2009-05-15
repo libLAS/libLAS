0cee405f3ff5d72e87532fb2fc2ebc437865d4c1
Howard Butler <hobu.inc@gmail.com>
2009-05-15 04:40:06 +0000

    more PC stuff
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%401264

--- apps/las2oci.cpp
+++ apps/las2oci.cpp
@@ -1,7 +1,84 @@
 #include "oci_wrapper.h"
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+OWStatement* Run(OWConnection* connection, ostringstream& command) 
+{
+    OWStatement* statement = 0;
+    statement = connection->CreateStatement(command.str().c_str());
+    
+    if (statement->Execute() == false) {
+        delete statement;
+        return 0;
+    }
+    
+    return statement;    
+}
+
+bool Cleanup(OWConnection* connection, const char* tableName)
+{
+    ostringstream oss;
+    OWStatement* statement = 0;
+    
+    oss << "DELETE FROM " << tableName;
+    statement = Run(connection, oss);
+    if (statement != 0) delete statement; else return false;
+    oss.str("");
+
+    oss << "DROP TABLE " << tableName;
+    cout << oss.str() << endl;
+    statement = Run(connection, oss);
+    if (statement != 0) delete statement; else return false;
+    oss.str("");    
+    
+    oss << "DROP TABLE BLKTAB ";
+    statement = Run(connection, oss);
+    if (statement != 0) delete statement; else return false;
+    oss.str("");
+    
+    oss << "DROP TABLE RES ";
+    statement = Run(connection, oss);
+    if (statement != 0) delete statement; else return false;
+    oss.str("");
+
+    oss << "DROP TABLE INPTAB ";
+    statement = Run(connection, oss);
+    if (statement != 0) delete statement; else return false;
+    oss.str("");
+
+    oss << "DROP TABLE LIDAR_DATA";
+    statement = Run(connection, oss);
+    if (statement != 0) delete statement; else return false;
+    oss.str("");    
+
+    oss << "DELETE FROM USER_SDO_GEOM_METADATA WHERE TABLE_NAME='"<< tableName << "'";
+    statement = Run(connection, oss);
+    if (statement != 0) delete statement; else return false;
+    oss.str("");
+
+    return true;
+    
+    
+}
 
 
+bool CreateTable(OWConnection* connection, const char* tableName)
+{
+    ostringstream oss;
+    OWStatement* statement = 0;
+    
+    oss << "CREATE TABLE "<< tableName <<" (ID VARCHAR2(24), VAL_D1 NUMBER, VAL_D2 NUMBER, VAL_D3 NUMBER)";
+    statement = Run(connection, oss);
+    if (statement != 0) delete statement; else return false;
+    oss.str("");
+    
+    return true;
+    
 
+}
 void usage() {}
 
 int main(int argc, char* argv[])
@@ -59,54 +136,56 @@ int main(int argc, char* argv[])
     
     OWConnection* con = new OWConnection("lidar","lidar","ubuntu/crrel.local");
     if (con->Succed()) printf("succeded");
+    
+    Cleanup(con, "base");
+    CreateTable(con, "inptab");
 
-
- int   iCol = 0;
-    char  szField[OWNAME];
-    int   hType = 0;
-    int   nSize = 0;
-    int   nPrecision = 0;
-    signed short nScale = 0;
-
-    char szColumnList[OWTEXT];
-    szColumnList[0] = '\0';
-    OCIParam* phDesc = NULL;
-
-    const char* pszVATName="base";
-    phDesc = con->GetDescription( (char*) pszVATName );
-    while( con->GetNextField(
-                phDesc, iCol, szField, &hType, &nSize, &nPrecision, &nScale ) )
-    {
-        printf("field ... %s",szField);
-        switch( hType )
-        {
-            case SQLT_FLT:
-                printf("float...\n");
-                break;
-            case SQLT_NUM:
-                printf ("number...\n");
-                break;
-            case SQLT_CHR:
-            case SQLT_AFC:
-            case SQLT_DAT:
-            case SQLT_DATE:
-            case SQLT_TIMESTAMP:
-            case SQLT_TIMESTAMP_TZ:
-            case SQLT_TIMESTAMP_LTZ:
-            case SQLT_TIME:
-            case SQLT_TIME_TZ:
-                printf ("character...\n");
-                break;
-            default:
-                CPLDebug("GEORASTER", "VAT (%s) Column (%s) type (%d) not supported"
-                    "as GDAL RAT", pszVATName, szField, hType );
-                break;
-        }
-        // strcpy( szColumnList, CPLSPrintf( "%s substr(%s,1,%d),",
-        //     szColumnList, szField, MIN(nSize,OWNAME) ) );
-
-        iCol++;
-    }
+ // int   iCol = 0;
+ //    char  szField[OWNAME];
+ //    int   hType = 0;
+ //    int   nSize = 0;
+ //    int   nPrecision = 0;
+ //    signed short nScale = 0;
+ // 
+ //    char szColumnList[OWTEXT];
+ //    szColumnList[0] = '\0';
+ //    OCIParam* phDesc = NULL;
+ // 
+ //    const char* pszVATName="base";
+ //    phDesc = con->GetDescription( (char*) pszVATName );
+ //    while( con->GetNextField(
+ //                phDesc, iCol, szField, &hType, &nSize, &nPrecision, &nScale ) )
+ //    {
+ //        printf("field ... %s",szField);
+ //        switch( hType )
+ //        {
+ //            case SQLT_FLT:
+ //                printf("float...\n");
+ //                break;
+ //            case SQLT_NUM:
+ //                printf ("number...\n");
+ //                break;
+ //            case SQLT_CHR:
+ //            case SQLT_AFC:
+ //            case SQLT_DAT:
+ //            case SQLT_DATE:
+ //            case SQLT_TIMESTAMP:
+ //            case SQLT_TIMESTAMP_TZ:
+ //            case SQLT_TIMESTAMP_LTZ:
+ //            case SQLT_TIME:
+ //            case SQLT_TIME_TZ:
+ //                printf ("character...\n");
+ //                break;
+ //            default:
+ //                CPLDebug("GEORASTER", "VAT (%s) Column (%s) type (%d) not supported"
+ //                    "as GDAL RAT", pszVATName, szField, hType );
+ //                break;
+ //        }
+ //        // strcpy( szColumnList, CPLSPrintf( "%s substr(%s,1,%d),",
+ //        //     szColumnList, szField, MIN(nSize,OWNAME) ) );
+ // 
+ //        iCol++;
+ //    }
 
 
 
