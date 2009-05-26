71d3834d085a819a8688feac934029fb900f66cf
Howard Butler <hobu.inc@gmail.com>
2009-05-26 18:32:32 +0000

    try to fix imports for Python < 2.5
    --HG--
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/trunk%401286

--- python/liblas/__init__.py
+++ python/liblas/__init__.py
@@ -5,9 +5,21 @@ version = get_version()
 HAVE_GDAL = bool(las.LAS_IsGDALEnabled())
 HAVE_LIBGEOTIFF = bool(las.LAS_IsLibGeoTIFFEnabled())
 
-from . import file
-from . import point
-from . import header
-from . import vlr
-from . import color
-from . import srs
+import sys
+
+version = sys.version_info[:3]
+
+if version[1] > 4:
+    from . import file
+    from . import point
+    from . import header
+    from . import vlr
+    from . import color
+    from . import srs
+else:
+    import file
+    import point
+    import header
+    import vlr
+    import color
+    import srs    
