0293541b5a7da950f43b0e220678b850a48d55d4
Howard Butler <hobu.inc@gmail.com>
2009-08-25 23:50:18 +0000

    backport r1342
    --HG--
    branch : 1.2
    extra : convert_revision : svn%3A1766ff46-f334-0410-ab20-d63176f87757/branches/1.2%401343

--- src/lasspatialreference.cpp
+++ src/lasspatialreference.cpp
@@ -58,8 +58,6 @@ LASSpatialReference& LASSpatialReference::operator=(LASSpatialReference const& r
 {
     if (&rhs != this)
     {
-        m_tiff = 0;
-        m_gtiff = 0;        
         SetVLRs(rhs.GetVLRs());
         GetGTIF();
     }
