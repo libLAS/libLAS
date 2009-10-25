95e8d46a8fef90259acf65211baa4568a8022c8e
Mateusz Loskot <mateusz@loskot.net>
2009-10-25 01:02:56 +0100

    Updated CTestConfig.cmake file with URL to CDash

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,3 +1,10 @@
+###############################################################################
+#
+# CTest configuration for libLAS project
+#
+# libLAS CDash: http://my.cdash.org/index.php?project=libLAS
+#
+###############################################################################
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
