88cd3aa033bd5344d3cc821e3c20c7d30d75dd9b
Howard Butler <hobu.inc@gmail.com>
2010-02-05 14:00:08 -0600

    fix up include

--- src/detail/reader/reader.cpp
+++ src/detail/reader/reader.cpp
@@ -39,7 +39,7 @@
  * OF SUCH DAMAGE.
  ****************************************************************************/
 
-#include <liblas/detail/reader/reader10.hpp>
+#include <liblas/detail/reader/reader.hpp>
 #include <liblas/detail/reader/header.hpp>
 #include <liblas/detail/reader/vlr.hpp>
 #include <liblas/detail/utility.hpp>
