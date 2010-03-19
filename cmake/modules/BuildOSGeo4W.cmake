c5c7843e60769d67b807a82b7ce17b50f21dc05c
Howard Butler <hobu.inc@gmail.com>
2010-03-19 11:43:56 -0500

    start on a some cmake to build osgeo4w packaging
    --HG--
    rename : cmake/modules/FindGDAL.cmake => cmake/modules/BuildOSGeo4W.cmake

---
+++ cmake/modules/BuildOSGeo4W.cmake
@@ -0,0 +1,45 @@
+###############################################################################
+#
+# OSGeo4W packaging
+#
+# On success, the macro sets the following variables:
+# GEOTIFF_FOUND       = if the library found
+# GEOTIFF_LIBRARIES   = full path to the library
+# GEOTIFF_INCLUDE_DIR = where to find the library headers also defined,
+#                       but not for general use are
+# GEOTIFF_LIBRARY     = where to find the PROJ.4 library.
+# GEOTIFF_VERSION     = version of library which was found, e.g. "1.2.5"
+#
+# Copyright (c) 2009 Mateusz Loskot <mateusz@loskot.net>
+#
+# Module source: http://github.com/mloskot/workshop/tree/master/cmake/
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+###############################################################################
+
+if(WIN32)
+    set(DEFAULT_LIB_SUBDIR lib)
+    set(DEFAULT_DATA_SUBDIR .)
+    set(DEFAULT_INCLUDE_SUBDIR include)
+
+    if (MSVC)
+        set(DEFAULT_BIN_SUBDIR bin)
+    else()
+        set(DEFAULT_BIN_SUBDIR .)
+    endif()
+else()
+    # Common locatoins for Unix and Mac OS X
+    set(DEFAULT_BIN_SUBDIR bin)
+    set(DEFAULT_LIB_SUBDIR lib)
+    set(DEFAULT_DATA_SUBDIR share/liblas)
+    set(DEFAULT_INCLUDE_SUBDIR include)
+endif()
+
+ ADD_CUSTOM_COMMAND(
+   OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/foo.c
+   COMMAND ${CMAKE_COMMAND} copy ${CMAKE_CURRENT_SOURCE_DIR}/bar.c ${CMAKE_CURRENT_BINARY_DIR}/foo.c
+   DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/bar.c
+   )
+ ADD_EXECUTABLE(foo foo.c)
