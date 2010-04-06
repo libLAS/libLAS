ba7112f355f4d452d720f6c90f1cada6091d741f
Howard Butler <hobu.inc@gmail.com>
2010-04-06 11:31:17 -0500

    __iter__ support for color, add tests

--- python/liblas/color.py
+++ python/liblas/color.py
@@ -45,10 +45,52 @@ import ctypes
 
 class Color(object):
     def __init__(self, red=0, green=0, blue=0, owned=True, handle=None):
+        """
+        :keyword red: Red value for the point
+        :type red: integer
+        :keyword green: Green value for the point
+        :type green: integer
+        :keyword blue: Blue value for the point
+        :type blue: integer
+        :keyword owned: flag to denote whether or not the instance owns its shadow
+        :type owned: boolean
+        :keyword handle: raw ctypes object
+        
+        >>> from liblas import color
+        >>> c = color.Color()
+        >>> c.red
+        0
+        >>> c.green
+        0
+        >>> c.blue
+        0
+
+        >>> c = color.Color(red = 123, blue = 125, green = 124)
+        >>> c.red
+        123
+        >>> c.green
+        124
+        >>> c.blue
+        125
+          
+        .. note::
+            RGB values should always be normalized to 16 bit values. For example,
+            when encoding an 8 bit per channel pixel, multiply each channel value
+            by 256 prior to storage in these fields. This normalization allows
+            color values from different camera bit depths to be accurately merged.
+        """
         if handle:
             self.handle = handle
         else:
             self.handle = core.las.LASColor_Create()
+        
+        if red:
+            self.red = red
+        if green:
+            self.green = green
+        if blue:
+            self.blue = blue
+            
         self.owned = owned
     def __del__(self):
         if self.owned:
@@ -59,17 +101,45 @@ class Color(object):
         return core.las.LASColor_GetRed(self.handle)
     def set_red(self, value):
         return core.las.LASColor_SetRed(self.handle, value)
-    red = property(get_red, set_red)
+    doc = "Red value of the color triple"
+    red = property(get_red, set_red, None, doc)
 
     def get_green(self):
         return core.las.LASColor_GetGreen(self.handle)
     def set_green(self, value):
         return core.las.LASColor_SetGreen(self.handle, value)
-    green = property(get_green, set_green)
+    doc = "Green value of the color triple"
+    green = property(get_green, set_green, None, doc)
     
     def get_blue(self):
         return core.las.LASColor_GetBlue(self.handle)
     def set_blue(self, value):
         return core.las.LASColor_SetBlue(self.handle, value)
-    blue = property(get_blue, set_blue)
+    doc = "Blue value of the color triple"
+    blue = property(get_blue, set_blue, None, doc)
 
+    def __iter__(self):
+        """Iterator support.  Values are returned in [R,G,B] order
+        
+          >>> for i in c:
+          ...     print i
+          123
+          124
+          125
+  
+          >>> list(c)
+          [123, 124, 125]
+        """
+        r = False
+        g = False
+        b = False
+        if not r:
+            r = True
+            yield self.red
+        if not g:
+            g = True
+            yield self.green
+        if not b:
+            b = True
+            yield self.blue
+            
