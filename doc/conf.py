c6e40a49ed5c8dcb1f1ea43007a064125699f5b4
Howard Butler <hobu.inc@gmail.com>
2010-05-17 14:53:25 -0500

    doxygen-related stuff

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ sys.path.append(os.path.abspath('../python'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'rst2pdf.pdfbuilder', 'breathe']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'rst2pdf.pdfbuilder']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -165,10 +165,10 @@ htmlhelp_basename = 'libLASdoc'
 
 
 breathe_projects = {
-    "class":"./api/xml"
+    "liblas":"/Users/hobu/hg/liblas-cmake/doc/api/xml"
     }
 
-breathe_default_project = "class"
+breathe_default_project = "liblas"
 
 
 pdf_documents = [
