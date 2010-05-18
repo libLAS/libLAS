c60c4f69cdb1b4b998ff0ebc06a1bb10613673de
Howard Butler <hobu.inc@gmail.com>
2010-05-18 14:22:39 -0500

    doc work

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,13 @@ sys.path.append(os.path.abspath('../python'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'rst2pdf.pdfbuilder']
+extensions = [  'sphinx.ext.autodoc', 
+                'sphinx.ext.doctest', 
+                'sphinx.ext.intersphinx', 
+                'sphinx.ext.todo', 
+                'sphinx.ext.ifconfig', 
+                'rst2pdf.pdfbuilder', 
+                'breathe']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
