d0df767b6a851a75ea733f4abad3ca25e7e338b7
Howard Butler <hobu.inc@gmail.com>
2010-06-11 14:03:10 -0500

    more cmake for osgeo4w

--- cmake/modules/BuildOSGeo4W.cmake
+++ cmake/modules/BuildOSGeo4W.cmake
@@ -27,6 +27,7 @@ set(OSGEO4W_DIRECTORIES
     ${OSGEO4W_PYTHON_DIR})
 
 
+
 add_custom_target(BuildOSGeo4WDirectories
   COMMAND ${CMAKE_COMMAND} -E echo "Building OSGeo4W install directories")
 
@@ -45,5 +46,40 @@ macro (MAKE_OSGEO4W_DIRECTORIES)
 
 endmacro(MAKE_OSGEO4W_DIRECTORIES)
 
+if (APPLE)
+    set(SO_EXT dylib)
+elseif(WIN32)
+    set(SO_EXT dll)
+else()
+    set(SO_EXT so)
+endif(APPLE)
+
+set(OSGEO4W_FILES
+    ${LIBLAS_BUILD_OUTPUT_DIRECTORY};${OSGEO4W_LIB_BIN_DIR}
+    ${libLAS_SOURCE_DIR};${OSGEO4W_DEVEL_INCLUDE_LIBLAS_DIR}
+    )
+
+add_custom_target(CopyOSGeo4WFiles
+  COMMAND ${CMAKE_COMMAND} -E echo "Copying OSGeo4W files...")
+
+add_dependencies( CopyOSGeo4WFiles BuildOSGeo4WDirectories   )
+
+macro (COPY_OSGEO4W_DIRECTORIES source destination)
+
+   message(STATUS "Copying ${source} to ${destination}...")
+   
+    STRING(REGEX REPLACE "/" "_" target "${destination}" )
+
+    add_custom_target(copy_${target}
+      COMMAND ${CMAKE_COMMAND} -E copy_directory ${source} ${destination})
+
+      add_dependencies( CopyOSGeo4WFiles copy_${target} )
+
+
+endmacro(COPY_OSGEO4W_DIRECTORIES source destination)    
 
 MAKE_OSGEO4W_DIRECTORIES()
+COPY_OSGEO4W_DIRECTORIES(${LIBLAS_BUILD_OUTPUT_DIRECTORY}/ ${OSGEO4W_LIB_BIN_DIR}/)
+COPY_OSGEO4W_DIRECTORIES(${libLAS_SOURCE_DIR}/include/liblas ${OSGEO4W_DEVEL_INCLUDE_LIBLAS_DIR}/)
+COPY_OSGEO4W_DIRECTORIES(${libLAS_SOURCE_DIR}/python/liblas/*.py ${OSGEO4W_PYTHON_DIR}/)
+
