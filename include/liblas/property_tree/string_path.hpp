e52065ea662d84bc09fe40bad9b7dcffdb69d2e9
Howard Butler <hobu.inc@gmail.com>
2010-08-24 10:57:56 -0500

    oops, botched the merge

--- include/liblas/property_tree/string_path.hpp
+++ include/liblas/property_tree/string_path.hpp
@@ -212,11 +212,7 @@ namespace liblas { namespace property_tree
           ++m_start;
         }
 
-<<<<<<< local
-        if(boost::optional<key_type> key = m_tr.get_value(part)) {
-=======
         if(::boost::optional<key_type> key = m_tr.get_value(part)) {
->>>>>>> other
             return *key;
         }
         BOOST_PROPERTY_TREE_THROW(ptree_bad_path("Path syntax error", *this));
