3f68d6a652a6645666e176c0af02751d1a97b32d
Howard Butler <hobu.inc@gmail.com>
2010-12-01 11:02:45 -0600

    remove commented cruft

--- src/detail/writer/point.cpp
+++ src/detail/writer/point.cpp
@@ -124,22 +124,5 @@ void Point::write(const liblas::Point& point)
     // }
 }
 
-// void Point::fill() 
-// {
-//     liblas::Point& p = m_point;
-// 
-//     m_record.x = static_cast<int32_t>(detail::sround(((p.GetX() - m_header->GetOffsetX()) / m_header->GetScaleX())));
-//     m_record.y = static_cast<int32_t>(detail::sround(((p.GetY() - m_header->GetOffsetY()) / m_header->GetScaleY())));
-//     m_record.z = static_cast<int32_t>(detail::sround(((p.GetZ() - m_header->GetOffsetZ()) / m_header->GetScaleZ())));
-// 
-//     Classification::bitset_type clsflags(p.GetClassification());
-//     m_record.classification = static_cast<uint8_t>(clsflags.to_ulong());
-// 
-//     m_record.intensity = p.GetIntensity();
-//     m_record.flags = p.GetScanFlags();
-//     m_record.scan_angle_rank = p.GetScanAngleRank();
-//     m_record.user_data = p.GetUserData();
-//     m_record.point_source_id = p.GetPointSourceID();
-// }
 
 }}} // namespace liblas::detail::reader
