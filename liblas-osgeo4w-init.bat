8075a43d34fd8fedbad9beaeeccf832ad5cb5f38
Howard Butler <hobu.inc@gmail.com>
2010-12-16 09:23:43 -0600

    don't set PYTHONPATH and PATH for everyone

--- liblas-osgeo4w-init.bat
+++ liblas-osgeo4w-init.bat
@@ -1,3 +1,1 @@
 SET GDAL_DATA=%OSGEO4W_ROOT%\apps\gdal-dev\share\gdal
-SET PYTHONPATH=%OSGEO4W_ROOT%\apps\gdal-dev\pymod;%PYTHONPATH%
-PATH=%OSGEO4W_ROOT%\apps\gdal-dev\bin;%OSGEO4W_ROOT%\bin;%PATH%
