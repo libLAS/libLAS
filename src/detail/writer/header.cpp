0c3be7b3268e7d335789d97999d60c11f237bd81
Michael P. Gerlek <mpg@flaxen.com>
2010-12-22 14:19:23 -0800

    set the high bit on point format, iff compressed mode

--- src/detail/writer/header.cpp
+++ src/detail/writer/header.cpp
@@ -222,7 +222,10 @@ void Header::write()
 
     // 16. Point Data Format ID
     n1 = static_cast<uint8_t>(m_header.GetDataFormatId());
-    detail::write_n(m_ofs, n1, sizeof(n1));
+    uint8_t n1tmp = n1;
+    if (m_header.IsCompressed())
+        n1tmp |= 0x80;
+    detail::write_n(m_ofs, n1tmp, sizeof(n1tmp));
 
     // 17. Point Data Record Length
     n2 = m_header.GetDataRecordLength();
