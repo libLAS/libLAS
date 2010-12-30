b874ae8538411248526b2ac35ae1edaf6147be8c
Howard Butler <hobu.inc@gmail.com>
2010-12-30 11:32:08 -0600

    add laszip to my default configuration

--- hobu-config.bat
+++ hobu-config.bat
@@ -7,22 +7,20 @@ set LIBLAS=D:\liblas
 set OSGEO4W=C:\OSGeo4W
 set BOOST=D:\boost\boost_1_44
 set ORACLE_HOME=%OSGEO4W%
-set LASZIP_ROOT=A:\dev\zip
+set LASZIP_ROOT=d:\laszip
 REM set BUILD_TYPE=RelWithDebInfo
 REM set BUILD_TYPE=Debug
 set BUILD_TYPE=Release
 
 set PATH=%OSGEO4W%\apps\gdal-dev\bin;%OSGEO4W%\bin;%PATH%
 
-REM    -DLASZIP_INCLUDE_DIR=%LASZIP_ROOT%\include ^
-REM    -DLASZIP_LIBRARY=%LASZIP_ROOT%\bin\Debug\Debug\liblaszip.lib ^
     
 cmake -G %G% ^
     -DBOOST_INCLUDEDIR=%BOOST% ^
     -DWITH_GDAL=ON ^
     -DWITH_GEOTIFF=ON ^
     -DWITH_ORACLE=ON ^
-    -DWITH_LASZIP=OFF ^
+    -DWITH_LASZIP=ON ^
     -DTIFF_INCLUDE_DIR=%OSGEO4W%\include ^
     -DTIFF_LIBRARY=%OSGEO4W%\lib\libtiff_i.lib ^
     -DGEOTIFF_INCLUDE_DIR=%OSGEO4W%\include ^
@@ -31,6 +29,8 @@ cmake -G %G% ^
     -DGDAL_LIBRARY=%OSGEO4W%\apps\gdal-dev\lib\gdal_i.lib ^
     -DORACLE_INCLUDE_DIR=%ORACLE_HOME%\include ^
     -DORACLE_OCI_LIBRARY=%ORACLE_HOME%\lib\oci.lib ^
+    -DLASZIP_INCLUDE_DIR=%LASZIP_ROOT%\include ^
+    -DLASZIP_LIBRARY=%LASZIP_ROOT%\bin\Release\liblaszip.lib ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DCMAKE_VERBOSE_MAKEFILE=OFF ^
     %LIBLAS%
