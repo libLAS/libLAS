eecfd9642879ba418ee520a6bd6cce368beb6c06
Howard Butler <hobu.inc@gmail.com>
2011-01-27 14:05:27 -0600

    turn tests on

--- hobu-config.bat
+++ hobu-config.bat
@@ -17,10 +17,10 @@ set PATH=%OSGEO4W%\apps\gdal-dev\bin;%OSGEO4W%\bin;%PATH%
     
 cmake -G %G% ^
     -DBOOST_INCLUDEDIR=%BOOST% ^
-    -DWITH_GDAL=ON ^
+    -DWITH_GDAL=ON= ^
     -DWITH_GEOTIFF=ON ^
     -DWITH_ORACLE=ON ^
-    -DWITH_LASZIP=ON ^
+    -DWITH_LASZIP=OFF ^
     -DTIFF_INCLUDE_DIR=%OSGEO4W%\include ^
     -DTIFF_LIBRARY=%OSGEO4W%\lib\libtiff_i.lib ^
     -DGEOTIFF_INCLUDE_DIR=%OSGEO4W%\include ^
@@ -29,9 +29,10 @@ cmake -G %G% ^
     -DGDAL_LIBRARY=%OSGEO4W%\apps\gdal-dev\lib\gdal_i.lib ^
     -DORACLE_INCLUDE_DIR=%ORACLE_HOME%\include ^
     -DORACLE_OCI_LIBRARY=%ORACLE_HOME%\lib\oci.lib ^
-    -DLASZIP_INCLUDE_DIR=d:\laszip\include ^
-    -DLASZIP_LIBRARY=d:\laszip\bin\Debug\laszip.lib ^
+    -DLASZIP_INCLUDE_DIR=%OSGEO4W%\include ^
+    -DLASZIP_LIBRARY=%OSGEO4W%\lib\laszip.lib ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DCMAKE_VERBOSE_MAKEFILE=OFF ^
     -DWITH_STATIC_LASZIP=FALSE ^
+    -DENABLE_CTEST=ON ^
     %LIBLAS%
