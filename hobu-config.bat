6b5a7861dab4c59561f15e18c509c5e1acc5b737
Howard Butler <hobu.inc@gmail.com>
2011-02-01 12:04:44 -0600

    reflect using OSGeo4W's LASzip

--- hobu-config.bat
+++ hobu-config.bat
@@ -8,11 +8,11 @@ set OSGEO4W=C:\OSGeo4W
 set BOOST=D:\boost\boost_1_44
 set ORACLE_HOME=%OSGEO4W%
 set LASZIP_ROOT=d:\laszip
-REM set BUILD_TYPE=RelWithDebInfo
-set BUILD_TYPE=Debug
+set BUILD_TYPE=RelWithDebInfo
+REM set BUILD_TYPE=Debug
 REM set BUILD_TYPE=Release
 
-set PATH=%OSGEO4W%\apps\gdal-dev\bin;%OSGEO4W%\bin;%PATH%
+set PATH=%OSGEO4W%\bin;%PATH%
 
     
 cmake -G %G% ^
@@ -20,13 +20,13 @@ cmake -G %G% ^
     -DWITH_GDAL=ON ^
     -DWITH_GEOTIFF=ON ^
     -DWITH_ORACLE=ON ^
-    -DWITH_LASZIP=OFF ^
+    -DWITH_LASZIP=ON ^
     -DTIFF_INCLUDE_DIR=%OSGEO4W%\include ^
     -DTIFF_LIBRARY=%OSGEO4W%\lib\libtiff_i.lib ^
     -DGEOTIFF_INCLUDE_DIR=%OSGEO4W%\include ^
     -DGEOTIFF_LIBRARY=%OSGEO4W%\lib\geotiff_i.lib ^
-    -DGDAL_INCLUDE_DIR=%OSGEO4W%\apps\gdal-dev\include ^
-    -DGDAL_LIBRARY=%OSGEO4W%\apps\gdal-dev\lib\gdal_i.lib ^
+    -DGDAL_INCLUDE_DIR=%OSGEO4W%\include ^
+    -DGDAL_LIBRARY=%OSGEO4W%\lib\gdal_i.lib ^
     -DORACLE_INCLUDE_DIR=%ORACLE_HOME%\include ^
     -DORACLE_OCI_LIBRARY=%ORACLE_HOME%\lib\oci.lib ^
     -DLASZIP_INCLUDE_DIR=%OSGEO4W%\include ^
