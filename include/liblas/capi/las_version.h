4dd684b24ae90955fc799bdf9c6537d755b15499
Howard Butler <hobu.inc@gmail.com>
2011-06-22 11:08:19 -0500

    bump version number in preparation for release

--- include/liblas/capi/las_version.h
+++ include/liblas/capi/las_version.h
@@ -44,7 +44,7 @@
 
 #ifndef LIBLAS_VERSION_MAJOR
 #define LIBLAS_VERSION_MAJOR    1
-#define LIBLAS_VERSION_MINOR    6
+#define LIBLAS_VERSION_MINOR    7
 #define LIBLAS_VERSION_REV      0
 #define LIBLAS_VERSION_BUILD    0
 #endif
@@ -54,11 +54,11 @@
 #endif
 
 #ifndef LIBLAS_RELEASE_DATE
-#define LIBLAS_RELEASE_DATE     20100201
+#define LIBLAS_RELEASE_DATE     20110623
 #endif
 
 #ifndef LIBLAS_RELEASE_NAME
-#define LIBLAS_RELEASE_NAME     "1.6.0"
+#define LIBLAS_RELEASE_NAME     "1.7.0"
 #endif
 
 #endif /* LAS_VERSION_H_INCLUDED */
