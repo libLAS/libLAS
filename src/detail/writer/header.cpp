a1fffed87059bc7191b66c3d2c7588a4f27b16d7
Howard Butler <hobu.inc@gmail.com>
2011-06-28 11:20:13 -0500

    put r3027 back again, ugh

--- src/detail/writer/header.cpp
+++ src/detail/writer/header.cpp
@@ -154,6 +154,7 @@ void Header::write()
             VariableRecord v;
             zpd.ConstructVLR(v);
             m_header.AddVLR(v);
+            m_header.SetDataOffset(m_header.GetDataOffset()+v.GetRecordLength());
 #else
             throw configuration_error("LASzip compression support not enabled in this libLAS configuration.");
 #endif
