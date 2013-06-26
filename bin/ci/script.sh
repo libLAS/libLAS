116e832e32277403d6ae017df0eb36ca54906ad8
Mateusz Loskot <mateusz@loskot.net>
2013-06-26 01:16:15 +0100

    Test with verbose output

--- bin/ci/script.sh
+++ bin/ci/script.sh
@@ -3,6 +3,15 @@
 source ./bin/ci/common.sh
 mkdir -p _build
 cd _build
+
+echo "$(tmstamp) *** script::cmake-config starting $(date) ***"
 cmake ..
+echo "$(tmstamp) *** script::cmake-config finished $(date) ***"
+
+echo "$(tmstamp) *** script::cmake-build make -j ${NUMTHREADS} $(date) ***"
 make -j ${NUMTHREADS}
-make test
+echo "$(tmstamp) *** script::cmake-build finished $(date) ***"
+
+echo "$(tmstamp) *** script::cmake-test starting $(date) ***"
+ctest -V --output-on-failure .
+echo "$(tmstamp) *** script::cmake-test finished $(date) ***"
