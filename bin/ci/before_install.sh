16761eb1451490cc75b18b57c530ae109c4db6dc
Howard Butler <hobu.inc@gmail.com>
2014-06-10 11:31:53 -0500

    add filesystem and system to Travis config

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -4,7 +4,7 @@ source ./bin/ci/common.sh
 echo "$(tmstamp) *** before_install::apt-get starting $(date) ***"
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo apt-get update -qq
-sudo apt-get install -qq cmake libboost-program-options-dev libboost-thread-dev
+sudo apt-get install -qq cmake libboost-program-options-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev
 echo "$(tmstamp) *** before_install::apt-get finished $(date) ***"
 gcc --version
 clang --version
