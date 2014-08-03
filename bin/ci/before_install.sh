2b08229876be9a9aa7bcd69c249cb50f1d7852cc
Howard Butler <hobu.inc@gmail.com>
2014-08-03 14:24:19 -0500

    more libgdal1h attempt

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -8,18 +8,19 @@ sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y
 sudo apt-get update -qq
 sudo apt-get install -qq cmake libboost-all-dev
 sudo apt-get install \
-    libgdal1h-dev \
+    libgdal1h \
     libgeos-dev \
     libgeos++-dev \
     libproj-dev \
     libxml2-dev \
-    libtiff4-dev
+    libtiff4-dev \
+    libgeotiff-dev 
 # install libgeotiff from sources
-wget http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.0.tar.gz
-tar -xzf libgeotiff-1.4.0.tar.gz
-cd libgeotiff-1.4.0
-./configure --prefix=/usr && make && sudo make install
-cd $TRAVIS_BUILD_DIR
+#wget http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.0.tar.gz
+#tar -xzf libgeotiff-1.4.0.tar.gz
+#cd libgeotiff-1.4.0
+#./configure --prefix=/usr && make && sudo make install
+#cd $TRAVIS_BUILD_DIR
 
 echo "$(tmstamp) *** before_install::apt-get finished $(date) ***"
 gcc --version
