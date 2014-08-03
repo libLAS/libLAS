c25cfbde63251b3e8476a8281722faeb268f0765
Howard Butler <hobu.inc@gmail.com>
2014-08-03 14:08:21 -0500

    fetch all of boost for Travis

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -4,8 +4,9 @@ source ./bin/ci/common.sh
 echo "$(tmstamp) *** before_install::apt-get starting $(date) ***"
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y
+#sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo apt-get update -qq
-sudo apt-get install -qq cmake libboost-program-options-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev
+sudo apt-get install -qq cmake libboost-all-dev
 sudo apt-get install \
     libgdal-dev \
     libgeos-dev \
