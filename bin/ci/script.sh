2ba8004bb2ac6d55742dc48cdd977a935e1d44b7
Howard Butler <howard@hobu.co>
2016-08-21 23:18:36 -0500

    set LD_LIBRARY_PATH for test invocation

--- bin/ci/script.sh
+++ bin/ci/script.sh
@@ -5,7 +5,7 @@ mkdir -p _build
 cd _build
 
 echo "$(tmstamp) *** script::cmake-config starting $(date) ***"
-cmake .. -DWITH_GDAL=ON -DWITH_GEOTIFF=ON
+cmake .. -DWITH_GDAL=ON -DWITH_GEOTIFF=ON -DCMAKE_BUILD_TYPE=Debug
 echo "$(tmstamp) *** script::cmake-config finished $(date) ***"
 
 echo "$(tmstamp) *** script::cmake-build make -j ${NUMTHREADS} $(date) ***"
@@ -13,5 +13,5 @@ make -j ${NUMTHREADS}
 echo "$(tmstamp) *** script::cmake-build finished $(date) ***"
 
 echo "$(tmstamp) *** script::cmake-test starting $(date) ***"
-ctest -V --output-on-failure .
+LD_LIBRARY_PATH=./bin/Debug ctest -V --output-on-failure .
 echo "$(tmstamp) *** script::cmake-test finished $(date) ***"
