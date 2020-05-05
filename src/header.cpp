0d3b8d75f371a6b7c605bbe5293091cb64a7e2d3
yyunon <yonsel.yuksel@gmail.com>
2020-05-05 16:11:50 +0200

    fix: Update use of boost::bind placeholders (#189)
    Fixes #186
    
    Co-authored-by: Yuksel Yonsel <yuksel@geosignum.com>

--- src/header.cpp
+++ src/header.cpp
@@ -54,7 +54,7 @@
 // boost
 #include <boost/cstdint.hpp>
 #include <boost/lambda/lambda.hpp>
-
+#include <boost/bind/bind.hpp>
 //std
 #include <algorithm>
 #include <fstream>
@@ -607,6 +607,7 @@ bool SameVLRs(std::string const& name, uint16_t id, liblas::VariableRecord const
 
 void Header::DeleteVLRs(std::string const& name, uint16_t id)
 {
+    using namespace boost::placeholders;
 
     m_vlrs.erase( std::remove_if( m_vlrs.begin(),
                                   m_vlrs.end(),
