7e9759b688180b32bf019fae939ebdb2c09de6e1
Nicklas Larsson <n_larsson@yahoo.com>
2025-11-08 15:28:33 +0100

    Avoid use of OSRFixupOrdering with GDAL >=2.5
    Closes https://github.com/libLAS/libLAS/issues/164

--- src/gt_wkt_srs.cpp
+++ src/gt_wkt_srs.cpp
@@ -301,7 +301,9 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
                 oSRS.SetFromUserInput(pszWKT);
                 oSRS.SetExtension( "PROJCS", "PROJ4",
                                    "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs" );
+#if GDAL_VERSION_NUM < GDAL_COMPUTE_VERSION(2,5,0)
                 oSRS.FixupOrdering();
+#endif
                 CPLFree(pszWKT);
                 pszWKT = NULL;
                 oSRS.exportToWkt(&pszWKT);
@@ -507,7 +509,9 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
         {
             char	*pszWKT;
             oSRS.morphFromESRI();
+#if GDAL_VERSION_NUM < GDAL_COMPUTE_VERSION(2,5,0)
             oSRS.FixupOrdering();
+#endif
             if( oSRS.exportToWkt( &pszWKT ) == OGRERR_NONE )
                 return pszWKT;
         }
@@ -1109,7 +1113,9 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
 /* ==================================================================== */
     char	*pszWKT;
 
+#if GDAL_VERSION_NUM < GDAL_COMPUTE_VERSION(2,5,0)
     oSRS.FixupOrdering();
+#endif
 
     if( oSRS.exportToWkt( &pszWKT ) == OGRERR_NONE )
         return pszWKT;
